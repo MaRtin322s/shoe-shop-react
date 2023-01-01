8f58d9ba79277737eac25825e2461f98f4655d6b
MaRtin322s <marti.sofroniev0408@gmail.com>
2023-01-01 14:43:13 +0200

    render home page and register page
    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Register from "./Register/Register";
 
 function App() {
     return (
         <div id="wrapper">
             <Header />
             <main>
-
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
             </main>
             <Footer />
         </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header>
-            <a id="logo" href="/"
-            >
+            <Link id="logo" to="/">
                 <img id="logo-img" src="./images/logo.png" alt="logo" />
-            </a>
+            </Link>
 
             <nav>
                 <div>
-                    <a href="">Dashboard</a>
-                    <a href="">Search</a>
+                    <Link to="/dashboard">Dashboard </Link>
+                    <Link to="/search">Search </Link>
                 </div>
 
-                <div class="user">
-                    <a href="">Add Pair</a>
-                    <a href="">Logout</a>
+                <div className="user">
+                    <Link to="/create">Add Pair </Link>
+                    <Link to="/logout">Logout </Link>
                 </div>
 
-                <div class="guest">
-                    <a href="">Login</a>
-                    <a href="">Register</a>
+                <div className="guest">
+                    <Link to="/login">Login </Link>
+                    <Link to="/register">Register </Link>
                 </div>
             </nav>
         </header>
